73abd0e433f6085589e0cc4fdc9be313c2755b31
Nolantrob <nolanrobertson97@gmail.com>
2025-06-05 15:39:17 -0400

    Add prompt loop to keep playing after the end of the first game

--- main.rb
+++ main.rb
@@ -6,6 +6,28 @@ require 'colorize'
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-mastermind = Game.new
+def begin_game
+  mastermind = Game.new
+  mastermind.play_game
+end
 
-mastermind.play_game
+loop do
+  yes_statements = %w[y yes sure ok okay yeah ye yea]
+  no_statements = %w[n no nope nah]
+
+  begin_game
+  loop do
+    print "\nPlay again? (Y/N): "
+    answer = gets.to_s.chomp
+    if yes_statements.include?(answer)
+      begin_game
+      next
+    end
+    if no_statements.include?(answer)
+      system 'clear'
+      exit
+    end
+
+    puts 'Invalid Choice: Please try again.'
+  end
+end
